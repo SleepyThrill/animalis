31c549b98e7e5001d670696c83b4d311edd98b50
Plipli Pill <38331@raphaeldisanto.com.br>
2026-05-19 17:27:57 -0300

    Update Main.java

--- app/Main.java
+++ app/Main.java
@@ -1,6 +1,15 @@
 package app;
 
+import enums.FurColor;
+
+import model.Cat;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Com certeza não é o gato da minha ex... :3
+        Cat cat1 = new Cat("Loki", 3, 4.5, FurColor.fromCode(4), true, "Salmon", 25, true);
+        cat1.displayInfo();
+
     }
 }
